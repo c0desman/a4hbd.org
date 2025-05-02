45b27feba4243cd6b33c9a7f86b0a2396a8e401c
c0desman <ic0desman@gmail.com>
2025-05-02 21:09:43 +0600

    Updated the apos

--- src/app/(FirstPage)/about/chairman-message/page.js
+++ src/app/(FirstPage)/about/chairman-message/page.js
@@ -19,15 +19,15 @@ export default function ChairmanMessage() {
         {/* Hero Section */}
         <div className="max-w-screen-xl mx-auto px-4 pt-10 lg:pt-15 pb-5 lg:pb-8">
           <p className="mt-6 text-base md:text-lg font-bold text-justify">
-          With profound gratitude to Allah Subhanu Wa Ta'ala for his endless blessings, Aid For Humanity (A4H) strives to empower Bangladesh's underprivileged communities. Our nation, teeming with millions and potential, faces challenges like poverty, injustice, and violence. At A4H, we're determined to make a lasting impact.<br /><br />
+          With profound gratitude to Allah Subhanu Wa Ta&apos;ala for his endless blessings, Aid For Humanity (A4H) strives to empower Bangladesh&apos;s underprivileged communities. Our nation, teeming with millions and potential, faces challenges like poverty, injustice, and violence. At A4H, we&apos;re determined to make a lasting impact.<br /><br />
 
         Education is our weapon against poverty. We champion sustainable development, offering escape routes from economic hardship. Access to healthcare is vital, and we work tirelessly to ensure a healthier population. Bridges of understanding and collaboration strengthen communities. Finally, by equipping individuals with valuable skills, we empower them to lead fulfilling lives.<br /><br />
 
-        Since the beginning, A4H has impacted over a thousand lives. In 2017, a vicious genocide by Myanmar’s army forced three out of every four Rohingya Muslims to flee their homes. Over a million of them-mostly women and children continue to live impoverished on a narrow strip of land in Bangladesh's southern district of Cox's Bazar. We are raising funds and supporting them from the beginning of their journey in our country.<br /><br />
+        Since the beginning, A4H has impacted over a thousand lives. In 2017, a vicious genocide by Myanmar&apos;s army forced three out of every four Rohingya Muslims to flee their homes. Over a million of them-mostly women and children continue to live impoverished on a narrow strip of land in Bangladesh&apos;s southern district of Cox&apos;s Bazar. We are raising funds and supporting them from the beginning of their journey in our country.<br /><br />
 
         We are deeply grateful to the NGO Affairs Bureau and Bangladeshi Government for their vital role in enabling our path in achieving our goals. We are fortunate to collaborate with international NGOs, who bring invaluable expertise and resources to the table. A heartfelt thanks to our generous donors, development partners, tireless volunteers and well-wishers who remain by A4H and help us make a difference.<br /><br />
 
-        The Prophet Muhammad (PBUH) teaches us a powerful lesson: "He saw a person enjoying himself in Paradise because he removed a tree from a pathway due to the inconvenience it was causing" [Muslim].  If alleviating a minor inconvenience earns such reward, imagine the blessings bestowed upon those who dedicate their lives to removing suffering, hunger, and sorrow from mankind.<br /><br />
+        The Prophet Muhammad (PBUH) teaches us a powerful lesson: &apos;He saw a person enjoying himself in Paradise because he removed a tree from a pathway due to the inconvenience it was causing&apos; [Muslim].  If alleviating a minor inconvenience earns such reward, imagine the blessings bestowed upon those who dedicate their lives to removing suffering, hunger, and sorrow from mankind.<br /><br />
 
         Dear brothers and sisters, let compassion be our guide. Together, with your continued support, we can build a brighter future for all. May Allah accept our endeavors. Ameen.
           </p>
